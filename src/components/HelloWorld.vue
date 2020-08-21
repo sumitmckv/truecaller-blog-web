1ac272fec8fcc7a0c04bffed169bf50d10604eb8
sumitmckv <sumitmckv14@gmail.com>
2020-08-21 09:21:15 +0530

    added tailwind css

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
+    <p class="text-blue-500">
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
@@ -111,7 +111,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+import {Component, Prop, Vue} from 'vue-property-decorator';
 
 @Component
 export default class HelloWorld extends Vue {
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  theme: {
+    fontFamily: {
+      mono: [
+        'Menlo',
+        'Monaco',
+        '"Lucida Console"',
+        'Consolas',
+        '"Liberation Mono"',
+        '"Courier New"',
+        'monospace',
+      ],
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      width: {
+        '72': '18rem',
+      },
+    },
+  },
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+  },
+  plugins: [],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: ['./components/**/*.vue', './views/**/*.vue'],
+  },
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+};
